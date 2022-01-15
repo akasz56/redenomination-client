bc79ca483a6b4133c3281d753dc88072e05430e7
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-01-15 22:52:22 +0700

    routes changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import ParticipantsRoutes from "./routes/ParticipantsRoutes";
-import AdminRoutes from "./routes/AdminRoutes";
+import { Routes, Route, Outlet } from 'react-router-dom';
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { ParticipantsProvider } from './contexts/ParticipantsContext';
+import { AdminProvider } from './contexts/AdminContext';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,6 +9,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Admin from "./pages/admin/Admin";
 import Participants from "./pages/participants/Participants";
+import Simulation from "./pages/admin/Simulation";
 
 export default function App() {
   return (
@@ -17,13 +19,14 @@ export default function App() {
 
         <Route path="/">
           <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="login" element={<Login />} />
 
-          <Route path="/admin" element={<AdminRoutes />} >
+          <Route path="simulations" element={<AdminRoutes />} >
             <Route index element={<Admin />} />
+            <Route path=":id(\\d+)" element={<Simulation />} />
           </Route>
 
-          <Route path="/participants" element={<ParticipantsRoutes />} >
+          <Route path="participants" element={<ParticipantsRoutes />} >
             <Route index element={<Participants />} />
           </Route>
         </Route>
@@ -32,4 +35,25 @@ export default function App() {
       <Footer />
     </>
   )
+}
+
+
+function AdminRoutes() {
+  return (
+    <AdminProvider>
+      <ProtectedRoute auth='admin'>
+        <Outlet />
+      </ProtectedRoute>
+    </AdminProvider>
+  );
+}
+
+function ParticipantsRoutes() {
+  return (
+    <ParticipantsProvider>
+      <ProtectedRoute auth='participant'>
+        <Outlet />
+      </ProtectedRoute>
+    </ParticipantsProvider>
+  );
 }
