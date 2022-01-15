da3dc617771201d626bc578b5ad1b361453e13b6
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-01-15 22:53:17 +0700

    auth offline dev changes and logo delete

--- src/adapters/Authentication.js
+++ src/adapters/Authentication.js
@@ -1,30 +1,36 @@
-// const serverURL = "https://carbide-bongo-338115.et.r.appspot.com/";
-const serverURL = "http://127.0.0.1:8000/";
+import serverURL from "./serverURL";
+
+const offlineResponse = {
+    status: 200,
+    data: { jwtToken: "yes" }
+}
 
 export async function connectAsAdmin(password) {
-    return await fetch(serverURL + "api/sessions/admins", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body:
-            JSON.stringify({
-                "password": password
-            })
-    }).then(response => response.json());
+    // return await fetch(serverURL + "api/sessions/admins", {
+    //     method: "POST",
+    //     headers: {
+    //         "Accept": "application/json",
+    //         "Content-Type": "application/json"
+    //     },
+    //     body:
+    //         JSON.stringify({
+    //             "password": password
+    //         })
+    // }).then(response => response.json());
+    return offlineResponse;
 }
 
 export async function connectAsParticipant(token) {
-    return await fetch(serverURL + "api/sessions/tokens", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body:
-            JSON.stringify({
-                "token": token
-            })
-    }).then(response => response.json());
+    // return await fetch(serverURL + "api/sessions/tokens", {
+    //     method: "POST",
+    //     headers: {
+    //         "Accept": "application/json",
+    //         "Content-Type": "application/json"
+    //     },
+    //     body:
+    //         JSON.stringify({
+    //             "token": token
+    //         })
+    // }).then(response => response.json());
+    return offlineResponse;
 }
