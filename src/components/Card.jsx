2e8ec73a2eacb7c70e38aa2969c30ff4989815c3
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-01-19 00:00:57 +0700

    card component update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,20 @@ import './Card.css';
 
 export default function Card(props) {
 
-    let element = <Button className="var">Beli</Button>
-    if (props.variant === 'done') {
-        element = <p className="var text-primary">Terjual</p>
-    } else if (props.variant === 'waiting') {
-        element = <p className="var text-danger">Belum Terjual</p>
-    }
+    let variant = <Button className="var">Beli</Button>
+    if (props.variant === 'wait') { variant = <p className="var text-danger">Belum Terjual</p> }
+    else if (props.variant === 'done') { variant = <p className="var text-primary">Terjual</p> }
+    else if (props.variant === 'decentralized') { variant = <Button className="var">Tanya Penjual</Button> }
+
+    const role = props.role ? props.role : "Title 0";
 
     return (
         <div className={"simulation-card " + props.className}>
-            <span className="fw-bold">Penjual 1</span>
-            <span className="price">Rp. 3700</span>
-            {element}
+            <span className="fw-bold">{role}</span>
+            <span className="price">
+                {props.children}
+            </span>
+            {variant}
         </div>
     )
 }
