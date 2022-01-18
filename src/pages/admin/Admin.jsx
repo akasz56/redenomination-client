d87ebc505fed1766f70ba18b3ba684754b9559fe
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-01-18 12:31:01 +0700

    admin page update

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -31,66 +31,39 @@ export default function Admin() {
         }
     }
 
-    if (simulations)
-        return (
-            <Container>
-                <div className="mt-5 header">
-                    <span className="fs-1">History Simulasi</span>
-                    <Button variant="primary" onClick={addBtnHandler}>Tambah Simulasi</Button>
-                </div>
+    return (
+        <Container>
+            <div className="mt-5 header">
+                <span className="fs-1">History Simulasi</span>
+                <Button variant="primary" onClick={addBtnHandler}>Tambah Simulasi</Button>
+            </div>
 
-                <Table responsive hover className="mt-3">
-                    <thead>
-                        <tr>
-                            <th width='50'>No</th>
-                            <th width='60%'>Jenis Simulasi</th>
-                            <th>Tanggal Dibuat</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {simulations.map((simulation, i) => (
+            <Table responsive hover className="mt-3">
+                <thead>
+                    <tr>
+                        <th width='50'>No</th>
+                        <th width='60%'>Jenis Simulasi</th>
+                        <th>Tanggal Dibuat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {simulations ?
+                        simulations.map((simulation, i) => (
                             <tr key={i} className='simulations' onClick={event => rowHandler(event, simulation.simulationID)}>
-                                <td className='number'>{simulation.simulationID}</td>
+                                <td className='number'>{i + 1}</td>
                                 <td>
                                     <h3>{simulation.simulationType}</h3>
                                     <span>{simulation.goodsType}, {simulation.inflationType}</span>
                                 </td>
                                 <td>{simulation.timeCreated}</td>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-                <Button variant="danger" onClick={logoutBtnHandler}>Log Out</Button>
-            </Container>
-        )
-    else
-        return (
-            <Container>
-                <div className="mt-5 header">
-                    <span className="fs-1">History Simulasi</span>
-                    <Button variant="primary" onClick={addBtnHandler}>Tambah Simulasi</Button>
-                </div>
-
-                <Table responsive hover className="mt-3">
-                    <thead>
-                        <tr>
-                            <th width='50'>No</th>
-                            <th width='60%'>Jenis Simulasi</th>
-                            <th>Tanggal Dibuat</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className='simulations'>
-                            <td className='number'>0</td>
-                            <td>
-                                <h3>Simulation Type</h3>
-                                <span>Goods Type, Inflation Type</span>
-                            </td>
-                            <td>Date</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                <Button variant="danger" onClick={logoutBtnHandler}>Log Out</Button>
-            </Container>
-        )
+                        ))
+                        :
+                        <tr><td colSpan={3}><h1 className='my-5 text-center'>Tidak ada Data</h1></td></tr>
+                    }
+                </tbody>
+            </Table>
+            <Button variant="danger" onClick={logoutBtnHandler}>Log Out</Button>
+        </Container>
+    )
 }
