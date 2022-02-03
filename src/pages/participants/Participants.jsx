e2185407469dd4580bc6fa5fe90b75e0fcc626b3
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-03 11:17:22 +0700

    double auction function added

--- src/pages/participants/Participants.jsx
+++ src/pages/participants/Participants.jsx
@@ -9,6 +9,7 @@ import { SellerAuctionScreen, BuyerAuctionScreen } from './double-auction/Double
 import { BuyerIdleDS, Lobby, PostPriceDS, SellerIdleDS } from './decentralized/Decentralized';
 import { capitalize, logout } from '../../Utils';
 import Label from '../../components/Label';
+import LoadingComponent from '../../components/Loading';
 
 export default function Participants() {
     const { state } = useLocation();
@@ -49,37 +50,43 @@ export default function Participants() {
         document.title = phaseData.simulationType;
 
         socket.on("serverMessage", res => {
-            if (res.status === 400) {
-                console.log(res)
+            if (res.status === 401) {
                 window.alert("Anda belum terdaftar dalam server, silahkan coba masukkan token partisipan lagi");
                 logout(() => { window.location.reload("/"); });
+            } else {
+                console.log(res)
             }
         })
 
         if (stage === 'ready') {
-            socket.on("readyCount", (res) => {
+            function readyCountHandler(res) {
                 if (res.numberOfReadyPlayer === res.totalPlayer) {
                     setData({ ...data, participantNumber: res.totalPlayer })
                     socket.emit("startPhase", { "phaseId": state.phases[0].id })
                     setTimer(minutes * 60);
                     setStage(firstStage);
                 }
-            });
-        } else if (stage === 'otwComplete') {
-            const myTotalProfit = profits.reduce((partialSum, a) => partialSum + a, 0)
-            socket.emit("collectProfit", { "myProfit": myTotalProfit })
-            socket.on("collectedProfit", res => {
-                console.log(res);
+            }
+            socket.on("readyCount", readyCountHandler);
+        }
+
+        else if (stage === 'otwComplete') {
+            const myTotalProfit = profits.reduce((partialSum, a) => partialSum + a, 0);
+            function collectedProfitHandler(res) {
                 if (res.profitCollection.length === data.participantNumber) {
                     const allProfit = res.profitCollection.reduce((prev, profit) => prev + profit.value, 0)
-                    const myReward = Math.round((myTotalProfit / allProfit) * res.simulationBudget)
+                    const myReward = 5000 + Math.round((myTotalProfit / allProfit) * res.simulationBudget)
                     setData({ ...data, rewards: myReward })
                     socket.off("collectedProfit")
                     setStage("complete");
                 }
-            });
-        } else if (stage === 'postPrice' && phaseData.simulationType === "Posted Offer") {
-            socket.on("postedOfferList", res => {
+            }
+            socket.emit("collectProfit", { "myProfit": myTotalProfit })
+            socket.on("collectedProfit", collectedProfitHandler);
+        }
+
+        else if (stage === 'postPrice' && phaseData.simulationType === "Posted Offer") {
+            function postedOfferListHandler(res) {
                 if (res.length === (data.participantNumber / 2)) {
                     setData({
                         ...data, seller: res.map((item, i) => {
@@ -94,17 +101,19 @@ export default function Participants() {
                     });
                     setStage("flashSale");
                 } else if (res.length > (data.participantNumber / 2)) {
-                    console.log("kelebihan pak");
-                    console.log(res)
                     // test purposes
                     socket.emit("finishPhase", { "phaseId": phaseData.currentPhase.id })
                     logout(() => { window.location.href = "/"; });
                 }
-            })
+            }
+            socket.on("postedOfferList", postedOfferListHandler);
         }
 
         const interval = setInterval(() => { if (timer) { setTimer(timer - 1) } }, 1000);
-        return () => clearInterval(interval);
+        return () => {
+            clearInterval(interval);
+            socket.off("serverMessage")
+        }
     });
 
     function phaseContinue(profit = 0) {
@@ -135,8 +144,9 @@ export default function Participants() {
         }
     }
 
-    if (stage === 'ready')
+    if (stage === 'ready') {
         return <Ready socket={socket} data={{ ...phaseData, ...data }} />
+    }
     else {
         switch (phaseData.simulationType) {
             case "Posted Offer":
@@ -166,50 +176,44 @@ function PostedOfferHandler(stage, timer, socket, data, phaseContinue) {
 
         case "complete":
         case "otwComplete":
-            if (data.role === "seller") { return <CompleteScreen data={data} /> }
-            else if (data.role === "buyer") { return <CompleteScreen data={data} /> }
-            else { return <BlankScreen lineNumber="193" /> }
+            return <CompleteScreen data={data} />
 
         default:
             return <BlankScreen lineNumber="196" />
     }
 }
 
-function DoubleAuctionHandler(stage, setStage, socket, data, phaseContinue) {
+function DoubleAuctionHandler(stage, timer, socket, data, phaseContinue) {
     switch (stage) {
         case "auctionScreen":
-            if (data.role === "seller") { return <SellerAuctionScreen socket={socket} data={data} setStage={setStage} phaseContinue={phaseContinue} /> }
-            else if (data.role === "buyer") { return <BuyerAuctionScreen socket={socket} data={data} setStage={setStage} phaseContinue={phaseContinue} /> }
+            if (data.role === "seller") { return <SellerAuctionScreen socket={socket} data={data} timer={timer} phaseContinue={phaseContinue} /> }
+            else if (data.role === "buyer") { return <BuyerAuctionScreen socket={socket} data={data} timer={timer} phaseContinue={phaseContinue} /> }
             else { return <BlankScreen lineNumber="205" /> }
 
         case "complete":
         case "otwComplete":
-            if (data.role === "seller") { return <CompleteScreen data={data} /> }
-            else if (data.role === "buyer") { return <CompleteScreen data={data} /> }
-            else { return <BlankScreen lineNumber="210" /> }
+            return <CompleteScreen data={data} />
 
         default:
             return <BlankScreen lineNumber="213" />
     }
 }
 
-function DecentralizedHandler(stage, setStage, socket, data, phaseContinue) {
+function DecentralizedHandler(stage, timer, socket, data, phaseContinue) {
     switch (stage) {
         case "postPriceDS":
-            if (data.role === "seller") { return <PostPriceDS socket={socket} data={data} setStage={setStage} /> }
-            else if (data.role === "buyer") { return <BuyerIdleDS socket={socket} data={data} setStage={setStage} /> }
+            if (data.role === "seller") { return <PostPriceDS socket={socket} data={data} timer={timer} /> }
+            else if (data.role === "buyer") { return <BuyerIdleDS socket={socket} data={data} timer={timer} /> }
             else { return <BlankScreen lineNumber="222" /> }
 
         case "listShops":
-            if (data.role === "seller") { return <SellerIdleDS socket={socket} data={data} setStage={setStage} phaseContinue={phaseContinue} /> }
-            else if (data.role === "buyer") { return <Lobby socket={socket} data={data} setStage={setStage} phaseContinue={phaseContinue} /> }
+            if (data.role === "seller") { return <SellerIdleDS socket={socket} data={data} timer={timer} phaseContinue={phaseContinue} /> }
+            else if (data.role === "buyer") { return <Lobby socket={socket} data={data} timer={timer} phaseContinue={phaseContinue} /> }
             else { return <BlankScreen lineNumber="227" /> }
 
         case "complete":
         case "otwComplete":
-            if (data.role === "seller") { return <CompleteScreen data={data} /> }
-            else if (data.role === "buyer") { return <CompleteScreen data={data} /> }
-            else { return <BlankScreen lineNumber="232" /> }
+            return <CompleteScreen data={data} />
 
         default:
             return <BlankScreen lineNumber="235" />
@@ -218,22 +222,29 @@ function DecentralizedHandler(stage, setStage, socket, data, phaseContinue) {
 
 
 function CompleteScreen({ data }) {
-    const kontak = {
-        nomorwa: "6289608703393"
+    const kontak = "6289608703393"
+
+    function roundNumber(number) {
+        const rounding = 100
+        return Math.floor(number / rounding) * rounding;
     }
 
     function generateLink() {
-        return "https://wa.me/" + kontak.nomorwa + "?text=Saya menerima reward sebesar " + data.rewards + " dalam simulasi " + data.simulationType + " (" + data.loginToken + ")"
+        return "https://wa.me/" + kontak + "?text=Saya menerima reward sebesar " + data.rewards + " dalam simulasi " + data.simulationType + " (" + data.loginToken + ")"
     }
 
     return (
         <Container className='text-center d-flex flex-column'>
             <h1 className='text-center mt-5'>Simulasi Selesai</h1>
-            <p className='mt-5'>
-                Selamat, anda mendapatkan hadiah sebesar
-                <span className='text-primary fw-bolder fs-1 d-block'>Rp. {data.rewards}</span>
-                Silahkan klik tombol dibawah untuk menghubungi peneliti
-            </p>
+            {(data.rewards) ?
+                <p className='mt-5'>
+                    Selamat, anda mendapatkan hadiah sebesar
+                    <span className='text-primary fw-bolder fs-1 d-block'>Rp. {roundNumber(data.rewards)}</span>
+                    Silahkan klik tombol dibawah untuk menghubungi peneliti
+                </p>
+                :
+                <LoadingComponent className="mx-auto my-5" />
+            }
 
             <div className="mx-auto mt-4">
                 <a href={generateLink()} className='btn btn-primary px-4 py-3 fs-3' target="_blank" rel="noreferrer">Kontak</a>
@@ -249,8 +260,7 @@ function CompleteScreen({ data }) {
     )
 }
 
-
-function BlankScreen({ lineNumber }) {
+function BlankScreen({ lineNumber = 0 }) {
     return (
         <div className="child">
             Blank Screen : {lineNumber}
--- src/pages/participants/double-auction/doubleAuction.jsx
+++ src/pages/participants/double-auction/doubleAuction.jsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap'
 import Label from '../../../components/Label'
-import { capitalize } from '../../../Utils'
+import { adjustPrice, capitalize, displayPrice } from '../../../Utils'
 
-export function SellerAuctionScreen({ socket, data, setStage, phaseContinue }) {
+export function SellerAuctionScreen({ socket, data, phaseContinue }) {
+    const [inputPrice, setInputPrice] = useState(0);
+    const [socketData, setSocketData] = useState({ minPrice: "-", maxPrice: "-" });
+    const [status, setStatus] = useState({ matched: false, profit: 0 });
+    const [isDone, setIsDone] = useState(false);
+
+    function cleanUp() {
+        setIsDone(false);
+        setInputPrice(0);
+        setStatus({ matched: false, profit: 0 });
+        setSocketData({ minPrice: "-", maxPrice: "-" });
+    }
+
+    useEffect(() => {
+        socket.on("doubleAuctionList", res => {
+            if (res) {
+                console.log(res)
+                setSocketData({
+                    minPrice: displayPrice(res.minPrice, data.currentPhase.phaseType),
+                    maxPrice: displayPrice(res.maxPrice, data.currentPhase.phaseType)
+                });
+                if (res.TrxOccurrence * 2 >= data.participantNumber) {
+                    setIsDone(true)
+                }
+            }
+        });
+
+        if (status.matched && isDone) {
+            cleanUp();
+            const myProfit = status.profit;
+            phaseContinue(myProfit)
+        }
+    }, [status, isDone, socket, phaseContinue, data]);
+
     function submitHandler(e) {
-        e.preventDefault()
+        e.preventDefault();
+        socket.emit("da:postSeller", {
+            sellerBargain: Number(inputPrice),
+            phaseId: data.currentPhase.id
+        });
+
+        socket.on("bidMatch", res => {
+            setStatus({
+                matched: res.match,
+                profit: res.transaction.price - adjustPrice(data.unitCost, data.currentPhase.phaseType)
+            });
+            console.log(res.transaction.price - data.unitCost)
+        });
     }
 
     return (
@@ -12,29 +58,42 @@ export function SellerAuctionScreen({ socket, data, setStage, phaseContinue }) {
             <section className='row my-5 py-5 border rounded-pill'>
                 <div className="col-md-4">
                     <p>Offer</p>
-                    <h1 className='text-primary fw-bolder'>Rp. {data.minPrice}</h1>
+                    <h1 className='text-primary fw-bolder'>Rp. {socketData.minPrice}</h1>
                 </div>
                 <div className="col-md-4">
                     <p><span className='fw-bolder'>Unit Cost</span> anda</p>
-                    <h1 className='text-primary fw-bolder'>Rp. {data.unitCost}</h1>
+                    <h1 className='text-primary fw-bolder'>Rp. {displayPrice(data.unitCost, data.currentPhase.phaseType)}</h1>
                 </div>
                 <div className="col-md-4">
                     <p>Bid</p>
-                    <h1 className='text-primary fw-bolder'>Rp. {data.maxPrice}</h1>
+                    <h1 className='text-primary fw-bolder'>Rp. {socketData.maxPrice}</h1>
                 </div>
             </section>
 
-            <Form onSubmit={submitHandler} className='mb-5'>
-                <Form.Group controlId="inputHarga">
-                    <Form.Label className='mb-3'>Masukkan <span className='fw-bolder'>harga kesepakatan</span> yang ingin anda tetapkan</Form.Label>
-                    <Form.Control type="number"
-                        className='text-center'
-                        required
-                        max={data.unitValue}
-                    />
-                </Form.Group>
-                <Button type="submit" className='mt-3 py-3 px-5 fs-4'>Tetapkan</Button>
-            </Form>
+            {status.matched ?
+                <p>menunggu partisipan lain...</p>
+                :
+                <Form onSubmit={submitHandler} className='mb-5'>
+                    <Form.Group controlId="inputHarga">
+                        <Form.Label className='mb-3'>Masukkan <span className='fw-bolder'>harga kesepakatan</span> yang ingin anda ajukan</Form.Label>
+                        {(data.currentPhase.phaseType !== "postRedenomPrice") ?
+                            <Form.Control type="number" className='text-center' defaultValue={inputPrice} required
+                                onChange={e => { setInputPrice(e.target.value) }}
+                                min={data.unitCost}
+                            />
+                            :
+                            <Form.Control type="number" className='text-center' defaultValue={inputPrice} required
+                                onChange={e => { setInputPrice(e.target.value) }}
+                                max={10}
+                                min={data.unitCost / 1000}
+                                step={0.001}
+                            />
+
+                        }
+                    </Form.Group>
+                    <Button type="submit" className='mt-3 py-3 px-5 fs-4'>Tetapkan</Button>
+                </Form>
+            }
 
             <Label
                 className="my-5 mx-auto"
@@ -46,40 +105,98 @@ export function SellerAuctionScreen({ socket, data, setStage, phaseContinue }) {
     )
 }
 
-export function BuyerAuctionScreen({ socket, data, setStage, phaseContinue }) {
+export function BuyerAuctionScreen({ socket, data, phaseContinue }) {
+    const [inputPrice, setInputPrice] = useState(0);
+    const [socketData, setSocketData] = useState({ minPrice: "-", maxPrice: "-" });
+    const [status, setStatus] = useState({ matched: false, profit: 0 });
+    const [isDone, setIsDone] = useState(false);
+
+    function cleanUp() {
+        setIsDone(false);
+        setInputPrice(0);
+        setStatus({ matched: false, profit: 0 });
+        setSocketData({ minPrice: "-", maxPrice: "-" });
+    }
+
+    useEffect(() => {
+        socket.on("doubleAuctionList", res => {
+            if (res) {
+                console.log(res)
+                setSocketData({
+                    minPrice: displayPrice(res.minPrice, data.currentPhase.phaseType),
+                    maxPrice: displayPrice(res.maxPrice, data.currentPhase.phaseType)
+                });
+                if (res.TrxOccurrence * 2 >= data.participantNumber) {
+                    setIsDone(true)
+                }
+            }
+        });
+
+        if (status.matched && isDone) {
+            cleanUp();
+            const myProfit = status.profit;
+            phaseContinue(myProfit)
+        }
+    }, [status, isDone, socket, phaseContinue, data]);
+
     function submitHandler(e) {
-        e.preventDefault()
+        e.preventDefault();
+        socket.emit("da:postBuyer", {
+            buyerBargain: Number(inputPrice),
+            phaseId: data.currentPhase.id
+        });
+
+        socket.on("bidMatch", res => {
+            setStatus({
+                matched: res.match,
+                profit: adjustPrice(data.unitValue, data.currentPhase.phaseType) - res.transaction.price
+            });
+            console.log(data.unitValue - res.transaction.price)
+        });
     }
 
     return (
         <Container className='text-center d-flex flex-column'>
-
             <section className='row my-5 py-5 border rounded-pill'>
                 <div className="col-md-4">
                     <p>Offer</p>
-                    <h1 className='text-primary fw-bolder'>Rp. {data.minPrice}</h1>
+                    <h1 className='text-primary fw-bolder'>Rp. {socketData.minPrice}</h1>
                 </div>
                 <div className="col-md-4">
                     <p><span className='fw-bolder'>Unit Value</span> anda</p>
-                    <h1 className='text-primary fw-bolder'>Rp. {data.unitValue}</h1>
+                    <h1 className='text-primary fw-bolder'>Rp. {displayPrice(data.unitValue, data.currentPhase.phaseType)}</h1>
                 </div>
                 <div className="col-md-4">
                     <p>Bid</p>
-                    <h1 className='text-primary fw-bolder'>Rp. {data.maxPrice}</h1>
+                    <h1 className='text-primary fw-bolder'>Rp. {socketData.maxPrice}</h1>
                 </div>
             </section>
 
-            <Form onSubmit={submitHandler} className='mb-5'>
-                <Form.Group controlId="inputHarga">
-                    <Form.Label className='mb-3'>Masukkan <span className='fw-bolder'>harga kesepakatan</span> yang ingin anda tetapkan</Form.Label>
-                    <Form.Control type="number"
-                        className='text-center'
-                        required
-                        min={data.unitCost}
-                    />
-                </Form.Group>
-                <Button type="submit" className='mt-3 py-3 px-5 fs-4'>Tetapkan</Button>
-            </Form>
+            {status.matched ?
+                <p>menunggu partisipan lain...</p>
+                :
+                <Form onSubmit={submitHandler} className='mb-5'>
+                    <Form.Group controlId="inputHarga">
+                        <Form.Label className='mb-3'>Masukkan <span className='fw-bolder'>harga kesepakatan</span> yang ingin anda ajukan</Form.Label>
+                        {(data.currentPhase.phaseType !== "postRedenomPrice") ?
+                            <Form.Control type="number" className='text-center' defaultValue={inputPrice} required
+                                onChange={e => { setInputPrice(e.target.value) }}
+                                max={data.unitValue}
+                                min={0}
+                            />
+                            :
+                            <Form.Control type="number" className='text-center' defaultValue={inputPrice} required
+                                onChange={e => { setInputPrice(e.target.value) }}
+                                max={data.unitValue / 1000}
+                                min={0}
+                                step={0.001}
+                            />
+                        }
+
+                    </Form.Group>
+                    <Button type="submit" className='mt-3 py-3 px-5 fs-4'>Tetapkan</Button>
+                </Form>
+            }
 
             <Label
                 className="my-5 mx-auto"
@@ -90,4 +207,3 @@ export function BuyerAuctionScreen({ socket, data, setStage, phaseContinue }) {
         </Container>
     )
 }
-
