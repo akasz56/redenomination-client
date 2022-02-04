87df237c6328629a045f4ffff877070cde2bd6c9
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-04 18:48:23 +0700

    server change

--- src/adapters/SocketIO.jsx
+++ src/adapters/SocketIO.jsx
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000/");
+const socket = io("https://carbide-bongo-338115.et.r.appspot.com/");
+// const socket = io("http://localhost:8000/");
 
 export default socket;
--- src/adapters/serverURL.jsx
+++ src/adapters/serverURL.jsx
@@ -1,5 +1,5 @@
-// const mainURL = "https://carbide-bongo-338115.et.r.appspot.com/";
-const mainURL = "http://localhost:8000/";
+const mainURL = "https://carbide-bongo-338115.et.r.appspot.com/";
+// const mainURL = "http://localhost:8000/";
 
 export default mainURL
 export const apiURL = mainURL + "api/";
