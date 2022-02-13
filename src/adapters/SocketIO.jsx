2002a7acca1c2a7ada548f01d454806329e252b1
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-13 17:56:00 +0700

    serverURL update

--- src/adapters/SocketIO.jsx
+++ src/adapters/SocketIO.jsx
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://carbide-bongo-338115.et.r.appspot.com/");
-// const socket = io("http://localhost:8000/");
 const socket = io("https://api.experimentaleconomics-ipb.my.id/");
 
 export default socket;
--- src/adapters/serverURL.jsx
+++ src/adapters/serverURL.jsx
@@ -1,5 +1,3 @@
-// const mainURL = "https://carbide-bongo-338115.et.r.appspot.com/";
-// const mainURL = "http://localhost:8000/";
 const mainURL = "https://api.experimentaleconomics-ipb.my.id/";
 
 export default mainURL
