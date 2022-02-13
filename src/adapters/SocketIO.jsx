117e17668b2a9d920046f6015ea70db002598275
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-13 16:05:23 +0700

    http to https

--- src/adapters/SocketIO.jsx
+++ src/adapters/SocketIO.jsx
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // const socket = io("https://carbide-bongo-338115.et.r.appspot.com/");
 // const socket = io("http://localhost:8000/");
-const socket = io("http://api.experimentaleconomics-ipb.my.id/");
+const socket = io("https://api.experimentaleconomics-ipb.my.id/");
 
 export default socket;
--- src/adapters/serverURL.jsx
+++ src/adapters/serverURL.jsx
@@ -1,6 +1,6 @@
 // const mainURL = "https://carbide-bongo-338115.et.r.appspot.com/";
 // const mainURL = "http://localhost:8000/";
-const mainURL = "http://api.experimentaleconomics-ipb.my.id/";
+const mainURL = "https://api.experimentaleconomics-ipb.my.id/";
 
 export default mainURL
 export const apiURL = mainURL + "api/";
