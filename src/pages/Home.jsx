9bceaa57b1e8360accece33dde585357b553bdae
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-15 08:03:43 +0700

    auto capslock on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Container, Form, Button } from 'react-bootstrap';
 import socket from '../adapters/SocketIO';
 import LoadingComponent from '../components/Loading';
 import './Home.css';
-import { saveAuth } from '../Utils';
+import { printLog, saveAuth } from '../Utils';
 
 export default function Home() {
     const [loading, setLoading] = useState(false);
@@ -33,7 +33,7 @@ export default function Home() {
                     window.location.reload()
                 }
             } else {
-                console.log(res)
+                printLog(res)
                 const msg = "(" + res.status + ") " + res.message;
                 window.alert(msg);
                 setLoading(false);
@@ -49,11 +49,11 @@ export default function Home() {
                 <Form className="mt-3 row" onSubmit={handleSubmit}>
                     <Form.Group controlId="login" className='col-md-6'>
                         <Form.Label className='required'>Masukkan token partisipan anda disini</Form.Label>
-                        <Form.Control type="text" required placeholder="Token" value={token} onChange={e => setToken(e.target.value)} />
+                        <Form.Control type="text" required placeholder="Token" style={{ textTransform: "uppercase" }} value={token} onChange={e => setToken(e.target.value)} />
                     </Form.Group>
                     <Form.Group controlId="username" className='col-md-6'>
                         <Form.Label className='required'>Masukkan username yang anda dapat disini</Form.Label>
-                        <Form.Control type="text" required placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+                        <Form.Control type="text" required placeholder="Username" style={{ textTransform: "uppercase" }} value={username} onChange={e => setUsername(e.target.value)} />
                     </Form.Group>
                     <div className='position-relative mt-3 w-100'>
                         <Button className="position-absolute end-0 py-2 px-4" variant="primary" type="submit">Masuk</Button>
