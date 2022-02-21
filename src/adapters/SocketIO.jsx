e47c66e956f3ab17427305db548f68feae881fdf
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-21 15:18:17 +0700

    socket upgrade false

--- src/adapters/SocketIO.jsx
+++ src/adapters/SocketIO.jsx
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 import mainURL from "./serverURL";
 
-const socket = io(mainURL)
+const socket = io(mainURL, {
+    transports: ["polling"],
+    upgrade: false,
+})
 
 export default socket;
