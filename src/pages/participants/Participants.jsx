48f47258e75f7ecdc508eb7798346df273723a50
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-26 13:52:08 +0700

    relogin refactor

--- src/pages/participants/Participants.jsx
+++ src/pages/participants/Participants.jsx
@@ -4,7 +4,7 @@ import socket from "../../adapters/SocketIO";
 import ReadyScreenHandler from "./ReadyScreenHandler";
 import CompleteScreenHandler from "./CompleteScreenHandler";
 import BlankScreen from "./BlankScreen";
-import { alertUser } from "../../Utils";
+import { alertUser, checkIfLoggedIn, logout, saveAuth } from "../../Utils";
 import PhaseHandler from "./PhaseHandler";
 
 export const participantStage = {
@@ -18,31 +18,26 @@ export default function Participants() {
     const [stateData, setStateData] = useState(state);
     const [stateStage, setStateStage] = useState(participantStage.READY);
 
+    function retryLogin() {
+        socket.emit("loginToken", { "token": state.detail.loginToken.toUpperCase(), "username": state.detail.username });
+        socket.on("serverMessage", res => {
+            if (res.status === 200 && res.data.isSessionRunning) {
+                setStateData(res.data);
+            } else { alertUser(res) }
+        })
+    }
+
     useEffect(() => {
         function serverMessageHandler(res) {
-            if (res.status === 401) {
-                const loggedIn = checkIfLoggedIn();
-                if (loggedIn) {
-                    socket.emit("loginToken", { "token": loggedIn.token.toUpperCase(), "username": loggedIn.username });
-                    socket.once("serverMessage", res => {
-                        if (res.status === 200) {
-                            if (res.data.isSessionRunning) {
-                                saveAuth("participant", { token: loggedIn.token, username: loggedIn.username, });
-                                setStateData(res.data)
-                            } else {
-                                window.alert("Simulasi belum dijalankan");
-                                logout(() => { window.location.reload() })
-                            }
-                        } else {
-                            alertUser(res)
-                        }
-                    })
-                }
-            } else if (res.status >= 300) {
-                alertUser(res)
-            }
+            if (res.status === 401) { retryLogin() }
+            else if (res.status === 403) { retryLogin() }
+            else if (res.status >= 300) { alertUser(res); console.log("alertUser", res) }
+            else { console.log("serverMsg", res) }
         }
         socket.on("serverMessage", serverMessageHandler)
+
+        function sessionDataUpdateHandler(res) { console.log("sessionDataUpdate", res); }
+        socket.on("sessionDataUpdate", sessionDataUpdateHandler);
 
         return () => {
             socket.off("serverMessage")
